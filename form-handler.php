70b1328e6d0ef842bc7a56f8703c477c4b6ab689
vprune2 <122639690+vprune2@users.noreply.github.com>
2024-12-01 20:34:51 -0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,4 +4,23 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+$email_from = 'vero.sol21@outlook.com' 
+
+$email_subject = 'New Form Submission'
+
+$email_body = "User Name: $name.\n".
+              "User Email: $email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'vero.sol21@outlook.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Contact.html");
+
 ?>
